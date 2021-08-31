6ff6e7fc849c5afec1ef75a68978a45424c76f80
Fernando <48518539+mobilepadawan@users.noreply.github.com>
2021-08-31 15:54:44 -0300

    Exercise 01

--- Crear Elementos de forma Dinámica/js/elementosdinamicos.js
+++ Crear Elementos de forma Dinámica/js/elementosdinamicos.js
@@ -1,20 +1,27 @@
-const body = document.getElementById("cuerpo")
+const body = document.querySelector("body")
 
 const titulo1 = document.createElement("h1")
 titulo1.innerText = "EduITfy"
-body.append(titulo1)
+body.appendChild(titulo1)
 
 const parrafo1 = document.createElement("p")
 parrafo1.innerText = "Nos distendemos con un poco de música"
-document.body.appendChild(parrafo1)
+body.appendChild(parrafo1)
 
 const imagen = document.createElement("img")
 imagen.src = "cover/blank.jpg"
-imagen.width = "300px"
-document.body.appendChild(imagen)
+//SIEMPRE VOY A LO SEGURO, PERO EN IMG NO PUDE HACER ANDAR ESTO
+// imagen.width = "300px"
+// imagen.height = "300px"
+imagen.setAttribute("width", "300px")
+imagen.setAttribute("height", "300px")
+body.appendChild(imagen)
 
 const audio = document.createElement("audio")
 audio.src = "Sources/Tones_and_I_Dance_Monkey.mp3"
 audio.controls = true
 audio.buffered = true
-document.body.appendChild(audio)
+audio.autoplay = true
+audio.loop = true
+body.appendChild(audio)
+
