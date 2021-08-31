202bcf96057a6f8643d22d3c8c6014964c3132fe
Fernando O <fernando@vidamobile.com.ar>
2021-08-31 13:44:07 -0300

    Updating exercices

--- Acceder a Elementos de una página/js/elementos.js
+++ Acceder a Elementos de una página/js/elementos.js
@@ -8,11 +8,19 @@ document.querySelector("h1").innerText = titulo
 document.querySelector("p").innerText = parrafo1
 //document.getElementsByTagName("p")[1].innerText = ""
 
-//EJERCICIO BASICO 2 (PINTAR LOS PARAGRAPHS CON LETRA AZUL)
+//EJERCICIO BASICO 2 (GENERAR UN ARRAY DE UN MISMO ELEMENTO HTML)
 const parrafosEnAzul = document.querySelectorAll("p")
-parrafosEnAzul.length // me devuelve el total de elementos HTML del tipo <p>
+console.log(`Total de elementos <p>: ${parrafosEnAzul.length}`) // me devuelve el total de elementos HTML del tipo <p>
 
-//EJERCICIO BASICO 3
+//EJERCICIO BASICO 3 (APLICAR LA FUENTE DE COLOR AZUL A LOS ELEMENTOS HTML ANTERIORES)
+for (let el of parrafosEnAzul)
+   el.classList.add("blue-text")
+
+   //EXPLICAR QUE, FOR... OF ES UNA EVOLUCIÓN DE FOR (LET I = 0;...;...) {}
+
+   //Y QUE TAMBIÉN EXISTE FOR... IN, PERO QUE EN ESTE CASO DEBEMOS USAR UN INDICE
+
+//EJERCICIO BASICO 4
 //debugger
 // function cargarProductos() {
 //     let listado = ''
